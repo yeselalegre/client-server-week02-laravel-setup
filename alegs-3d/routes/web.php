c883a395f98b04f96124963e0eb7a9eaa5113273
Yesel Ann Alegre <yesel.alegre0206@gmail.com>
2026-08-05 08:13:53 +0800

    censored: student id

--- alegs-3d/routes/web.php
+++ alegs-3d/routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome', [
         'studentName'   => 'Yesel Ann M. Alegre',
-        'studentNumber' => '0124-1126',
+        'studentNumber' => '012*-****',
         'course'        => 'BSIT',
         'section'       => '3D',
         'subject'       => 'ITST 302',
